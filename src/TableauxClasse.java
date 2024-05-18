fdb61de5b1a2f7c4a63d92dbd1cae65d0ea636f5
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-19 00:21:48 +0200

    initial commit

--- src/TableauxClasse.java
+++ src/TableauxClasse.java
@@ -1,27 +1,34 @@
+import java.util.Arrays;
 public class TableauxClasse {
     public static void main(String[] args) {
-        int[]chiffreAffaire;
+//        int[]chiffrAffaire;
 
         //création du tableau et taille du tableau
 
-        chiffreAffaire = new int[12];
+//        chiffrAffaire = new int[12];
 
-        //initialisation du premier élément
-        chiffreAffaire [1] = 1234;
-        chiffreAffaire [2] = 8798;
-        chiffreAffaire [3] = 4545;
-        chiffreAffaire [4] = 4141;
-        chiffreAffaire [5] = 8989;
-        chiffreAffaire [6] = 3636;
-        chiffreAffaire [7] = 5656;
-        chiffreAffaire [8] = 2222;
-        chiffreAffaire [9] = 3202;
-        chiffreAffaire [10] = 2828;
-        chiffreAffaire [11] = 7896;
-        chiffreAffaire [12] = 5631;
+//        //initialisation du premier élément
+//        chiffrAffaire [1] = 1234;
+//        chiffrAffaire [2] = 8798;
+//        chiffrAffaire [3] = 4545;
+//        chiffrAffaire [4] = 4141;
+//        chiffrAffaire [5] = 8989;
+//        chiffrAffaire [6] = 3636;
+//        chiffrAffaire [7] = 5656;
+//        chiffrAffaire [8] = 2222;
+//        chiffrAffaire [9] = 3202;
+//        chiffrAffaire [10] = 2828;
+//        chiffrAffaire [11] = 7896;
+//        chiffrAffaire [12] = 5631;
 
-        //Affichage
 
-//        System.out.println();
+        //Création et initialisation du tableau
+        int[]chiffrAffaire = {1234, 568,2541,25,1664,38,9873,35876,746,956,2563,45653};
+
+        //Trie du tableau dans l'ordre croissant
+        Arrays.sort(chiffrAffaire);
+
+        //Affichage du tableau
+        System.out.println(Arrays.binarySearch(chiffrAffaire,  38));
     }
 }
