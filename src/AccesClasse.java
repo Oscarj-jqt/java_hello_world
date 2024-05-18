48294e36f0e373bc1c790d74c2d63bfd2f6dac48
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-18 19:38:07 +0200

    initial commit

--- src/AccesClasse.java
+++ src/AccesClasse.java
@@ -1,17 +1,17 @@
-public class AccesClasse {
-    public static void main(String[] args) {
-        //Private --> accès au sein de la classe
-        private int nombre;
-
-        //protected --> utilisable dans la classe où la variable est définie
-        protected float taux;
-
-        //public --> accessible partout
-        public double niveau;
-
-        //static --> associé à un des mots clés précédents
-        //transformation en variable de classe
-
-        public static String transformation;
-    }
-}
+//public class AccesClasse {
+//    public static void main(String[] args) {
+//        //Private --> accès au sein de la classe
+//        private int nombre;
+//
+//        //protected --> utilisable dans la classe où la variable est définie
+//        protected float taux;
+//
+//        //public --> accessible partout
+//        public double niveau;
+//
+//        //static --> associé à un des mots clés précédents
+//        //transformation en variable de classe
+//
+//        public static String transformation;
+//    }
+//}
---
+++ src/TypeClasse.java
@@ -0,0 +1,16 @@
+public class TypeClasse {
+    public static void main(String[] args) {
+        //déclaration de variable
+        double distance;
+        int conversion;
+
+        //affectation de valeur ) la variable distance
+        distance = 2546.78;
+
+        //conversion du type double en type int
+        conversion = (int)distance;
+
+        //On vérifie
+        System.out.println("La valeur est "+conversion);
+    }
+}
