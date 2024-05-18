2479bd501e937e9fce76203dab51355ea376652f
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-18 18:55:21 +0200

    initial commit

--- src/MaPremiereClasse.java
+++ src/MaPremiereClasse.java
@@ -1,6 +1,10 @@
 public class MaPremiereClasse {
     public static void main(String[] args) {
-        // ceci est un commentaire
+        // ceci est un commentaire simple
+        /* commentaire multiligne */
+        /** Commentaire de type doc
+         *
+         */
         //Pour afficher le code on tape System.out.print...
         System.out.println("Hello World !");
         System.out.println("Mon premier programme Java.");
---
+++ src/TutorialClasse.java
@@ -0,0 +1,11 @@
+public class TutorialClasse {
+    public static void main(String[] args) {
+        //identifiant valide
+        int valeur = 10;
+
+        // On affiche la variable créée précedemment
+        System.out.println(valeur);
+        // avec concaténation
+        System.out.println("La valeur est "+valeur+".");
+    }
+}
