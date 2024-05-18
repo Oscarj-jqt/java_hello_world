bbd8ac88c073580194b05ad79bf05e64087cda33
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-18 18:42:20 +0200

    initial commit

---
+++ src/MaPremiereClasse.java
@@ -0,0 +1,8 @@
+public class MaPremiereClasse {
+    public static void main(String[] args) {
+        // ceci est un commentaire
+        //Pour afficher le code on tape System.out.print...
+        System.out.println("Hello World !");
+        System.out.println("Mon premier programme Java.");
+    }
+}
