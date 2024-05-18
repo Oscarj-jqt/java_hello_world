775da625b92333ceb8daa780010f5b66801f3ac5
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-18 19:06:14 +0200

    initial commit

---
+++ src/Variable.java
@@ -0,0 +1,18 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        //types numériques entiers
+
+        int in = 10000;
+        byte bt = 14;
+        short sh = 123;
+        long lg = 123712883;
+
+        //Afficher la valeur de ces variables
+
+        System.out.println("La variable de in est: "+in+".");
+        System.out.println("La variable de byte est: "+in+".");
+        System.out.println("La variable de sh est: "+sh+".");
+        System.out.println("La variable de lg est: "+lg+".");
+    }
+}
