501477d60ac09664384d3c7212722df79f2f9a1e
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-18 19:42:54 +0200

    initial commit

---
+++ src/ConstanceClasse.java
@@ -0,0 +1,18 @@
+public class ConstanceClasse
+{
+    public static void main(String[] args) {
+
+        //Initialisation et déclaration de lac constante
+        final double TAUXTVA = 1.196;
+
+        //Déclaration et intialisation des autres variables
+        int prixHT = 10;
+        double prixTTC;
+
+        //Utilisation de la constante
+        prixTTC = prixHT * TAUXTVA;
+
+        //Calcule
+        System.out.println(prixTTC);
+    }
+}
