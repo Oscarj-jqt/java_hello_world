b1063554fb997e2fd99f4f8ef7a73abf4a8da89d
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-18 19:18:27 +0200

    initial commit

--- src/Caractères.java
+++ src/Caractères.java
@@ -1,5 +1,20 @@
 public class Caractères {
     public static void main(String[] args) {
 
+
+        //Déclaration d'une variable
+
+        int nombre;
+        double taux;
+        String prenom;
+
+        //Utilisation des variables
+        nombre = 23;
+        taux = 3.3;
+        prenom = "Oscar";
+
+        //Affichage
+
+        System.out.println("Je suis "+prenom+" et j'ai "+nombre+" ans. Mon taux est de "+taux+".");
     }
 }
